b907aa3370651e52dbade85b0be2114b0c08f161
Guilherme Branco <guilherme.branco@feedzai.com>
2025-11-19 12:19:51 +0000

    add integration tests

---
+++ src/test/java/guru/springframework/JavaHelloWorldIT.java
@@ -0,0 +1,19 @@
+/*
+ * The copyright of this file belongs to Feedzai. The file cannot be
+ * reproduced in whole or in part, stored in a retrieval system,
+ * transmitted in any form, or by any means electronic, mechanical,
+ * photocopying, or otherwise, without the prior permission of the owner.
+ *
+ * © 2025 Feedzai, Strictly Confidential
+ */
+
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+public class JavaHelloWorldIT {
+    @Test
+    void myFauxIntegration() {
+        System.out.println("My IT Ran");
+    }
+}
