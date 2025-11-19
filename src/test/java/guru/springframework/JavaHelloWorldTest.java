2cb8d1270bcb3c036bb1f44f0497d34460a405d5
Guilherme Branco <guilherme.branco@feedzai.com>
2025-11-19 11:09:11 +0000

    add junit 5 test alongside junit 4

--- src/test/java/guru/springframework/JavaHelloWorldTest.java
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -5,12 +5,6 @@ import org.junit.Test;
 
 public class JavaHelloWorldTest {
 
-    // POJO test
-    public void testGetHello() {
-        JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
-        assert("Hello World".equals(javaHelloWorld.getHello()));
-    }
-
     @Test
     public void getHello() {
         JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
