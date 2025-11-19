33e15c4e1d4d450ebf156f8202779665a418e905
Guilherme Branco <guilherme.branco@feedzai.com>
2025-11-19 10:50:48 +0000

    add junit 4 test

--- src/test/java/guru/springframework/JavaHelloWorldTest.java
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -1,9 +1,18 @@
 package guru.springframework;
 
+
+import org.junit.Test;
+
 public class JavaHelloWorldTest {
 
     // POJO test
     public void testGetHello() {
+        JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
+        assert("Hello World".equals(javaHelloWorld.getHello()));
+    }
+
+    @Test
+    public void getHello() {
         JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
         assert("Hello World".equals(javaHelloWorld.getHello()));
     }
