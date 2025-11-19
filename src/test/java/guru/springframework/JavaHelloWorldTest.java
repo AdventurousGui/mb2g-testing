a57d02814146b3950599a7f3c90d1801216d89cc
Guilherme Branco <guilherme.branco@feedzai.com>
2025-11-19 10:50:22 +0000

    add test file

---
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -0,0 +1,10 @@
+package guru.springframework;
+
+public class JavaHelloWorldTest {
+
+    // POJO test
+    public void testGetHello() {
+        JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
+        assert("Hello World".equals(javaHelloWorld.getHello()));
+    }
+}
